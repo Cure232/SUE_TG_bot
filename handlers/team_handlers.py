a9df9673cd98020d289c58b1d14e4ce2b6098e7e
Buddy <Pro100biv@mail.ru>
2025-04-27 16:51:43 +0400

    added update_data for teammate 	feat: team_handlers.py added update_data

--- handlers/team_handlers.py
+++ handlers/team_handlers.py
@@ -1,4 +1,6 @@
-from aiogram import F, Router
+import os 
+
+from aiogram import F, Bot, Router
 from aiogram.types import Message
 from aiogram.filters import StateFilter
 from aiogram.fsm.context import FSMContext
@@ -18,8 +20,12 @@ from log.logger import logger
 
 router = Router()
 
+IMG_DIR = "images"
+
 @router.message(F.text == LEXICON["team_button"], StateFilter(RegistrationSoloFSM.team_or_solo))
 async def process_team_registration(message: Message, state: FSMContext):
+    logger.info(
+        f"Пользователь @{message.from_user.username} начал создание команды ({message.from_user.id})")
     await state.update_data(team_id=None)
     await state.set_state(RegistrationSoloFSM.fill_team_name)
     await message.answer(
@@ -29,6 +35,8 @@ async def process_team_registration(message: Message, state: FSMContext):
 
 @router.message(StateFilter(RegistrationSoloFSM.fill_team_name))
 async def process_team_name_registration(message: Message, state: FSMContext):
+    logger.info(
+        f"Пользователь @{message.from_user.username} назвал команду ({message.from_user.id})")
     await state.update_data(team_name=message.text)
     await state.clear()
     await message.answer(
@@ -38,6 +46,8 @@ async def process_team_name_registration(message: Message, state: FSMContext):
 
 @router.message(F.text == LEXICON["add_teammate_button"])
 async def process_teammate_start_registration(message: Message, state: FSMContext):
+    logger.info(
+        f"Пользователь @{message.from_user.username} начал добавление игрока ({message.from_user.id})")
     await state.set_state(RegistrationTeamFSM.fill_name)
     await message.answer(
         "Введите ФИО игрока: ",
@@ -46,7 +56,9 @@ async def process_teammate_start_registration(message: Message, state: FSMContex
 
 @router.message(StateFilter(RegistrationTeamFSM.fill_name))
 async def process_teammate_group_registration(message: Message, state: FSMContext):
-    await state.update_data()
+    await state.update_data(name=message.txt)
+    logger.info(
+        f"ФИО тиммейта {message.text} сохранено для пользователя @{message.from_user.username} ({message.from_user.id})")
     await state.set_state(RegistrationTeamFSM.fill_group)
     await message.answer(
         "Введите группу игрока: ",
@@ -55,6 +67,9 @@ async def process_teammate_group_registration(message: Message, state: FSMContex
 
 @router.message(StateFilter(RegistrationTeamFSM.fill_group))
 async def process_teammate_steam_registration(message: Message, state: FSMContext):
+    await state.update_data(group_num=message.txt)
+    logger.info(
+        f"Номер группы тиммейта {message.text} сохранен для пользователя @{message.from_user.username} ({message.from_user.id})")
     await state.update_data()
     await state.set_state(RegistrationTeamFSM.fill_steam_lnk)
     await message.answer(
@@ -64,6 +79,9 @@ async def process_teammate_steam_registration(message: Message, state: FSMContex
 
 @router.message(StateFilter(RegistrationTeamFSM.fill_steam_lnk))
 async def process_teammate_photo_registartion(message: Message, state: FSMContext):
+    await state.update_data(steam_link=message.txt)
+    logger.info(
+        f"Ссылка на STEAM тиммейта {message.text} сохранена для пользователя @{message.from_user.username} ({message.from_user.id})")
     await state.update_data()
     await state.set_state(RegistrationTeamFSM.fill_photo)
     await message.answer(
@@ -72,12 +90,24 @@ async def process_teammate_photo_registartion(message: Message, state: FSMContex
     )
 
 @router.message(StateFilter(RegistrationTeamFSM.fill_photo))
-async def process_teammate_end_regisation(message: Message, state: FSMContext):
+async def process_teammate_end_regisation(message: Message, state: FSMContext, bot: Bot):
+    photo = message.photo[-1]
+    file = await bot.get_file(photo.file_id)
+    file_path = os.path.join(IMG_DIR, f"{photo.file_id}.jpg")
+    os.makedirs(IMG_DIR, exist_ok=True)
+    await bot.download_file(file.file_path, file_path)
+    await state.update_data(st_card_photo=file_path)
+    await state.update_data(name=message.txt)
+    logger.info(
+        f"Пользователь @{message.from_user.username} ({message.from_user.id}) загрузил фото студенческого тиммейта.")
     await state.update_data()
     await message.amswer(
         "Тиммейт добавлен", 
         reply_markup=teammates_keyboard
     )
+    logger.info(
+        f"Пользователь добавил тиммейта @{message.from_user.username} ({message.from_user.id})")
+
     await state.clear()
 
 @router.message(F.text == LEXICON["team_done_button"])
